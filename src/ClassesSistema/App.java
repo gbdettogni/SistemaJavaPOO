f9b46c62d5a48c80b0ccbc668830d5cc0fbb858d
heitorps <heitorps2020@gmail.com>
2025-02-12 13:52:13 -0300

    Imprimindo todas as pessoas

--- src/ClassesSistema/App.java
+++ src/ClassesSistema/App.java
@@ -7,6 +7,6 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws Exception {
         List<Pessoa> pessoas = LeituraPrincipal.lePessoa(args[0]);
-        PessoaFisica.imprimeSujeito((PessoaFisica) pessoas.getFirst());
+        for(Pessoa p : pessoas) p.imprimeSujeito();
     }
 }
