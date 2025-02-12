f5594ddd5aa31844e608726f936980d67c71fb11
heitorps <heitorps2020@gmail.com>
2025-02-12 13:53:49 -0300

    Constructors de pessoas feitos

--- src/ClassesSistema/PessoaJuridica.java
+++ src/ClassesSistema/PessoaJuridica.java
@@ -1,10 +1,17 @@
 package ClassesSistema;
 
+import java.util.Locale;
+
 public class PessoaJuridica extends Pessoa {
     private String cnpj;
 
-    public void setCnpj(String cnpj) {
+    public  PessoaJuridica(String id, String nome, String telefone, String endereco, String cnpj){
+        super(id,nome,telefone,endereco);
         this.cnpj = cnpj;
     }
 
+    public void imprimeSujeito(){
+        Locale l = Locale.FRANCE;
+        System.out.printf(l,"Loja: %s\nEndereço: %s\nTelefone: %s\nCNPJ: %s\n",nome,endereco,telefone,cnpj);
+    }
 }
