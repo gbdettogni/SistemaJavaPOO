4399539a63d641f498da16b6c2956496baf64344
gbdettogni <gustavobdettogni@gmail.com>
2025-02-13 15:57:32 -0300

    Heitor mudou uns negócios

---
+++ src/ClassesLeitura/PlanilhaCasais.java
@@ -0,0 +1,41 @@
+package ClassesLeitura;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import ClassesSistema.Casal;
+import ClassesSistema.Pessoa;
+import ClassesSistema.PessoaFisica;
+
+public class PlanilhaCasais {
+    static List<Casal> _lePlanilhaCasais(String pasta, List<Pessoa> pessoas){
+        List<Casal> casais = new ArrayList<>();
+
+        try {   //primeiro pela planilha de lares
+            
+            Scanner leitor = new Scanner(new File(pasta + "lares.csv"));
+            leitor.useDelimiter("\n");
+            while (leitor.hasNextLine()) {
+                String linha = leitor.nextLine();
+                String[] dados = linha.split(";");
+
+                String idLar = dados[0];
+                //
+                //
+                //  TO DO: leitura dos lares
+                //
+                //
+            }
+
+            leitor.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Erro de I/O");
+            System.exit(0);
+        }
+
+        return casais;
+    }
+}
